af5b39d6bfd869b8d61bf4cd51c8788049b4c764
TarcisioNeto98 <tarcisioneto1998@gmail.com>
2019-08-21 22:18:17 -0300

    App android que abre uma pagina web

---
+++ web_page/app/src/main/java/com/tarcisio/web_page/MainActivity.java
@@ -0,0 +1,30 @@
+package com.tarcisio.web_page;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button botao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        botao = findViewById(R.id.bWeb);
+        botao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.google.com"));
+                startActivity(intent);
+            }
+        });
+    }
+}
